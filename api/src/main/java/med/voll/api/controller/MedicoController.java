95808305a983ddf15d9c17d29845e4b3816d8a2c
Renato <rena.f.oliveira@gmail.com>
2022-11-12 18:17:14 -0300

    add validations with Bean Validation

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,6 @@
 package med.voll.api.controller;
 
-import ch.qos.logback.core.net.SyslogOutputStream;
+import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
@@ -22,7 +22,7 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody DadosCadastroMedico dados) {
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         medicoRepository.save(new Medico(dados));
     }
 
